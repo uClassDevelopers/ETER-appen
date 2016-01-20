77427c9aecd364d6df9c9a9f14ce6d1ce8913cd8
Adam Jacobs <jacobs_feld@hotmail.com>
2016-01-20 12:49:24 +0100

    Revert "change language"
    This reverts commit 3fe4b7182ab0aca61701cf4e5e76befaeccb567b.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -101,7 +101,7 @@ function fixCordovaYoutubePlayers() {
 // modules
 angular.module('eter.controllers', ['ngSanitize'])
 
-.controller('FrontCtrl', function($scope, $http, $ionicSlideBoxDelegate, $state, $translate) {
+.controller('FrontCtrl', function($scope, $http, $ionicSlideBoxDelegate, $state) {
 	// get school ids
 	var schoolsIdsTemp = [];
 	$http({
@@ -122,21 +122,13 @@ angular.module('eter.controllers', ['ngSanitize'])
 	$scope.submitSchoolId = function(id) {
 		console.log("adding school id: " + id);
 		app.checkForSchoolOrAdd(id);
-		
-		$.getJSON( "http://eter.rudbeck.info//eter-app-api/?apikey=vV85LEH2cUJjshrFx5&oto_directory=1", function( response ) {
-			$.each( response.oto_directory, function( key, schoolObj ) {
-				if(schoolObj.school_id == id) {
-					$translate.use(schoolObj.lang);
-				}
-			});
-		});
 		$state.go('tab.start');
 	}
 	
 })
 
-.controller('StartCtrl', ['$scope', '$http', '$ionicSlideBoxDelegate', '$state', '$translate', function($scope, $http, $ionicSlideBoxDelegate, $state, $translate) {
-	$scope.slideHasChanged = function() {
+.controller('StartCtrl', function($scope, $http, $ionicSlideBoxDelegate, $state) {
+    $scope.slideHasChanged = function() {
         $ionicSlideBoxDelegate.$getByHandle('image-viewer').update();
     };
     $scope.$on("$ionicView.beforeEnter", function() {
@@ -309,7 +301,7 @@ angular.module('eter.controllers', ['ngSanitize'])
             $('#uclass').html('<p class="text-danger" style="text-align: center;">.</p>');
             console.log(data);
         });
-}])
+})
 
 .controller('GuidesCtrl', function($scope, $http) {
     $scope.$on("$ionicView.beforeEnter", function() {
