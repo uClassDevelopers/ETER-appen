1bb57294dd98555c0c4a804273c6107875f5bb3e
admin <information@d-ank.com>
2016-06-30 15:17:16 +0200

    Revert "Fix websql"
    This reverts commit 9ab344a3135b279b18d2fa03859723f39af9827f.

--- www/js/app.js
+++ www/js/app.js
@@ -28,8 +28,8 @@ var eter = angular.module('eter', ['ionic', 'eter.controllers', 'eter.services',
 eter.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     //Start pushwoosh
-    initPushwoosh();
-    app.initialize();
+      initPushwoosh();
+      app.initialize();
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
@@ -39,8 +39,6 @@ eter.run(function($ionicPlatform) {
       // org.apache.cordova.statusbar required
       StatusBar.styleLightContent();
     }
-    app.openDatabase();
-    app.createTable();
   });
 })
 
@@ -53,22 +51,22 @@ eter.config(['$stateProvider', '$urlRouterProvider', '$translateProvider', funct
   $stateProvider
 
   // setup an abstract state for the tabs directive
+  
 
-
-    .state('tab', {
+  .state('tab', {
     url: "/tab",
     abstract: true,
     templateUrl: "templates/tabs.html"
   })
   // front page
-    .state('front', {
-    url: '/front',
-    templateUrl: 'templates/front-page.html',
-    controller: 'FrontCtrl'
+  .state('front', {
+	url: '/front',
+	templateUrl: 'templates/front-page.html',
+	controller: 'FrontCtrl'
   })
-
+  
   // Each tab has its own nav history stack:
-    .state('tab.start', {
+  .state('tab.start', {
     url: '/start',
     views: {
       'tab-start': {
@@ -77,36 +75,36 @@ eter.config(['$stateProvider', '$urlRouterProvider', '$translateProvider', funct
       }
     }
   })
-    .state('tab.start-detail', {
-    url: '/start/:pid',
-    views: {
-      'tab-start': {
-        templateUrl: 'templates/start-detail.html',
-        controller: 'GuidesDetailCtrl'
+  .state('tab.start-detail', {
+      url: '/start/:pid',
+      views: {
+        'tab-start': {
+          templateUrl: 'templates/start-detail.html',
+          controller: 'GuidesDetailCtrl'
+        }
       }
-    }
-  })
+    })
 
-    .state('tab.guides', {
-    url: '/guides',
-    views: {
-      'tab-guides': {
-        templateUrl: 'templates/tab-guides.html',
-        controller: 'GuidesCtrl'
+  .state('tab.guides', {
+      url: '/guides',
+      views: {
+        'tab-guides': {
+          templateUrl: 'templates/tab-guides.html',
+          controller: 'GuidesCtrl'
+        }
       }
-    }
-  })
+    })
     .state('tab.guides-detail', {
-    url: '/guides/:pid',
-    views: {
-      'tab-guides': {
-        templateUrl: 'templates/guides-detail.html',
-        controller: 'GuidesDetailCtrl'
+      url: '/guides/:pid',
+      views: {
+        'tab-guides': {
+          templateUrl: 'templates/guides-detail.html',
+          controller: 'GuidesDetailCtrl'
+        }
       }
-    }
-  })
+    })
 
-    .state('tab.courses', {
+  .state('tab.courses', {
     url: '/courses',
     views: {
       'tab-courses': {
@@ -115,17 +113,17 @@ eter.config(['$stateProvider', '$urlRouterProvider', '$translateProvider', funct
       }
     }
   })
-    .state('tab.courses-detail', {
-    url: '/courses/:pid',
-    views: {
-      'tab-courses': {
-        templateUrl: 'templates/courses-detail.html',
-        controller: 'GuidesDetailCtrl'
+  .state('tab.courses-detail', {
+      url: '/courses/:pid',
+      views: {
+        'tab-courses': {
+          templateUrl: 'templates/courses-detail.html',
+          controller: 'GuidesDetailCtrl'
+        }
       }
-    }
-  })
+    })
 
-    .state('tab.eter', {
+  .state('tab.eter', {
     url: '/eter',
     views: {
       'tab-eter': {
@@ -134,98 +132,98 @@ eter.config(['$stateProvider', '$urlRouterProvider', '$translateProvider', funct
       }
     }
   })
-    .state('tab.eter-detail', {
-    url: '/eter/:eterId',
-    views: {
-      'tab-eter': {
-        templateUrl: 'templates/eter-detail.html',
-        controller: 'EterDetailCtrl'
+  .state('tab.eter-detail', {
+      url: '/eter/:eterId',
+      views: {
+        'tab-eter': {
+          templateUrl: 'templates/eter-detail.html',
+          controller: 'EterDetailCtrl'
+        }
       }
-    }
-  });
+    });
 
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/start');
-
-
-
+  
+	
+	
   $translateProvider.translations("ENG", {
-    START_TITLE: 'Start',
+	  START_TITLE: 'Start',
     GUIDES_TITLE: 'Guides',
-    COURSES_TITLE: 'Courses',
-    ABOUT_TITLE: 'About & Support',
-    NAV_BACK: 'Go back',
-    GUIDES_SLIDEOUT_LATEST: 'Latest',
-    GUIDES_SLIDEOUT_CATEGORIES: 'Categories',
-    GUIDES_SLIDEOUT_NOTREAD: 'Non-read',
-    GUIDES_SLIDEOUT_TEACHERS: 'Teachers',
-    GUIDES_SLIDEOUT_HISTORY: 'History',
-    GUIDES_SLIDEOUT_SEARCH: 'SEARCH',
-    COURSES_HEADER: "All Courses",
-    COURSE_SUBHEADER: "Course Elements",
-    OTO_APP_NAME: "The OTO-app",
-    OTO_APP_PRESENTATION: "The OTO-app (One-To-One) was created by the uClass developers: Adam Feldstein Jacobs and Daniel Holm.",
-    READ_MORE_UCLASS: "Find out more about uClass.",
-    FORM_CONTACT: "Contact",
-    FORM_NAME: "Your name",
-    FORM_SCHOOL_MAIL: "School mail",
-    FORM_OPINIONS: "Opinions (Must be filled in)",
-    FORM_TYPE_OF_OPINION: "What is the message about?",
-    FORM_TYPE_OPINION: "Opinion on app",
-    FORM_TYPE_GUIDE: "Guide suggestion",
-    FORM_TYPE_OTHER: "Other suggestion",
-    FORM_SEND: "Send message"
+	  COURSES_TITLE: 'Courses',
+	  ABOUT_TITLE: 'About & Support',
+	  NAV_BACK: 'Go back',
+	  GUIDES_SLIDEOUT_LATEST: 'Latest',
+	  GUIDES_SLIDEOUT_CATEGORIES: 'Categories',
+	  GUIDES_SLIDEOUT_NOTREAD: 'Non-read',
+	  GUIDES_SLIDEOUT_TEACHERS: 'Teachers',
+	  GUIDES_SLIDEOUT_HISTORY: 'History',
+	  GUIDES_SLIDEOUT_SEARCH: 'SEARCH',
+	  COURSES_HEADER: "All Courses",
+	  COURSE_SUBHEADER: "Course Elements",
+	  OTO_APP_NAME: "The OTO-app",
+	  OTO_APP_PRESENTATION: "The OTO-app (One-To-One) was created by the uClass developers: Adam Feldstein Jacobs and Daniel Holm.",
+	  READ_MORE_UCLASS: "Find out more about uClass.",
+	  FORM_CONTACT: "Contact",
+	  FORM_NAME: "Your name",
+	  FORM_SCHOOL_MAIL: "School mail",
+	  FORM_OPINIONS: "Opinions (Must be filled in)",
+	  FORM_TYPE_OF_OPINION: "What is the message about?",
+	  FORM_TYPE_OPINION: "Opinion on app",
+	  FORM_TYPE_GUIDE: "Guide suggestion",
+	  FORM_TYPE_OTHER: "Other suggestion",
+	  FORM_SEND: "Send message"
   })
-    .translations("SWE", {
-    START_TITLE: 'Start',
+  .translations("SWE", {
+	  START_TITLE: 'Start',
     GUIDES_TITLE: 'Guider',
-    COURSES_TITLE: 'Kurser',
-    ABOUT_TITLE: 'Om & support',
-    NAV_BACK: 'Bakåt',
-    GUIDES_SLIDEOUT_LATEST: 'Senaste',
-    GUIDES_SLIDEOUT_CATEGORIES: 'Kategorier',
-    GUIDES_SLIDEOUT_NOTREAD: 'Oläst',
-    GUIDES_SLIDEOUT_TEACHERS: 'Lärare',
-    GUIDES_SLIDEOUT_HISTORY: 'Historik',
-    GUIDES_SLIDEOUT_SEARCH: 'SÖK',
-    COURSES_HEADER: "Alla kurser",
-    COURSE_SUBHEADER: "Kursmoment",
-    OTO_APP_NAME: 'OTO-appen',
-    OTO_APP_PRESENTATION: "OTO-appen (One-To-One) är skapad utav uClass Developers Daniel Holm och Adam Feldstein Jacobs.",
-    READ_MORE_UCLASS: "Läs mer om uClass.",
-    FORM_CONTACT: "Kontakt",
-    FORM_NAME: "Ditt namn",
-    FORM_SCHOOL_MAIL: "Din skolmail",
-    FORM_OPINIONS: "Synpunkter (måste anges)",
-    FORM_TYPE_OF_OPINION: "Vad gäller din synpunkt?",
-    FORM_TYPE_OPINION: "Synpunkt på appen",
-    FORM_TYPE_GUIDE: "Behov av ny guide",
-    FORM_TYPE_OTHER: "Annat förslag",
-    FORM_SEND: "Skicka meddelande"
+	  COURSES_TITLE: 'Kurser',
+	  ABOUT_TITLE: 'Om & support',
+	  NAV_BACK: 'Bakåt',
+	  GUIDES_SLIDEOUT_LATEST: 'Senaste',
+	  GUIDES_SLIDEOUT_CATEGORIES: 'Kategorier',
+	  GUIDES_SLIDEOUT_NOTREAD: 'Oläst',
+	  GUIDES_SLIDEOUT_TEACHERS: 'Lärare',
+	  GUIDES_SLIDEOUT_HISTORY: 'Historik',
+	  GUIDES_SLIDEOUT_SEARCH: 'SÖK',
+	  COURSES_HEADER: "Alla kurser",
+	  COURSE_SUBHEADER: "Kursmoment",
+	  OTO_APP_NAME: 'OTO-appen',
+	  OTO_APP_PRESENTATION: "OTO-appen (One-To-One) är skapad utav uClass Developers Daniel Holm och Adam Feldstein Jacobs.",
+	  READ_MORE_UCLASS: "Läs mer om uClass.",
+	  FORM_CONTACT: "Kontakt",
+	  FORM_NAME: "Ditt namn",
+	  FORM_SCHOOL_MAIL: "Din skolmail",
+	  FORM_OPINIONS: "Synpunkter (måste anges)",
+	  FORM_TYPE_OF_OPINION: "Vad gäller din synpunkt?",
+	  FORM_TYPE_OPINION: "Synpunkt på appen",
+	  FORM_TYPE_GUIDE: "Behov av ny guide",
+	  FORM_TYPE_OTHER: "Annat förslag",
+	  FORM_SEND: "Skicka meddelande"
   });
-
-  // Set language
+	
+	// Set language
   $.getJSON( "http://eter.rudbeck.info//eter-app-api/?apikey=vV85LEH2cUJjshrFx5&oto_directory=1", function( response ) {
-    console.log("lang get request complete");
-    $.each( response.oto_directory, function( key, schoolObj ) {
-      app.db.transaction(function (tx) {
-        tx.executeSql("SELECT * FROM schoolinfo", [], function(tx, rs) {
-          var row;
-          var rowlength = rs.rows.length;
-          if(rowlength > 0) {
-            for (var i = 0; i < rowlength; i++) {
-              row = rs.rows.item(i);
-              if(row.otoid == schoolObj.school_id) {
-                var language = schoolObj.lang;
-                $translateProvider.use(language);
-              }
-            }
-          } else {
-            console.log("no school selected");
-          }
-        }, app.onError);
-      });
-    });
+	  console.log("lang get request complete");
+	  $.each( response.oto_directory, function( key, schoolObj ) {
+			app.db.transaction(function (tx) {
+				tx.executeSql("SELECT * FROM schoolinfo", [], function(tx, rs) {
+					var row;
+					var rowlength = rs.rows.length;
+					if(rowlength > 0) {
+						for (var i = 0; i < rowlength; i++) {
+							row = rs.rows.item(i);
+							if(row.otoid == schoolObj.school_id) {
+								var language = schoolObj.lang;
+								$translateProvider.use(language);
+							}
+						}
+					} else {
+						console.log("no school selected");
+					}
+				}, app.onError);
+			});
+	  });
   });
   //set default preferred lang
   $translateProvider.preferredLanguage("ENG");
